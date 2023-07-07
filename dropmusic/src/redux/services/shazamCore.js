59dbc21f6f2e08d92a1302738cdbaf6224726fb0
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2023-07-07 12:49:18 +0000

    app updates

--- dropmusic/src/redux/services/shazamCore.js
+++ dropmusic/src/redux/services/shazamCore.js
@@ -25,13 +25,26 @@ export const shazamCoreApi = createApi({
     }),
     endpoints: (builder) => ({
         getTopCharts: builder.query({query: () => '/charts/world' }),
-        getSongDetails: builder.query({query: ({ songid }) => `/tracks/details?track_id=${songid}`}),        
+        getSongDetails: builder.query({query: ({ songid }) => `/tracks/details?track_id=${songid}`}),   
+        getSongsByGenre: builder.query({ query: (genre) => `/charts/genre-world?genre_code=${genre}` }),
+        getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` }),
+        getSongsBySearch: builder.query({ query: (searchTerm) => `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
+        getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` }),
+        getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),     
     }),
 });
 
 
 export const {
     useGetTopChartsQuery,
+    useGetSongsByGenreQuery,
+    useGetSongsByCountryQuery,
+    useGetSongsBySearchQuery,
+    useGetArtistDetailsQuery,
     useGetSongDetailsQuery,
     useGetSongRelatedQuery,
-} = shazamCoreApi;
+  } = shazamCoreApi;
+
+
+
+
